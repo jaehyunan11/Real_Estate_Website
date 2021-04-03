89fa682e74bbe9b63dde570b5c58a59cf0bb630d
jaehyun An <jaehyuna11@gmail.com>
2021-04-02 19:20:12 -0700

    message update

--- real_estate/accounts/views.py
+++ real_estate/accounts/views.py
@@ -47,14 +47,30 @@ def register(request):
 def login(request):
     if request.method == 'POST':
         # Register User
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
+        # user is not found
+        else:
+            messages.error(request, 'Invalid credentials')
+            return redirect('login')
         return
     else:
         return render(request, 'accounts/login.html')
 
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, "You are now logged out")
     return redirect('index')
 
 
 def dashboard(request):
-    return redner(request, 'accounts/dashboard.html')
+    return render(request, 'accounts/dashboard.html')
