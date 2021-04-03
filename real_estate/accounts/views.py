c632f99e292755709e71ebba7d581d15c5185656
jaehyun An <jaehyuna11@gmail.com>
2021-04-02 18:27:50 -0700

    alert message and login register

--- real_estate/accounts/views.py
+++ real_estate/accounts/views.py
@@ -1,12 +1,45 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
-# Create your views here.
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 
 def register(request):
     if request.method == 'POST':
-        messages.error(request, 'Testing Error message')
-        return redirect('register')
+        # GET form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if passwords match
+        if password == password2:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is already existed')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is already existed')
+                    return redirect('register')
+                else:
+                    # Looks Good
+                    user = User.objects.create_user(
+                        username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    # Login after register
+                    # auth.login(request, user)
+                    # messages.success(request, 'You are now logged in')
+                    # return redirect('index')
+                    user.save()
+                    messages.success(
+                        request, 'You are now registered and please log in')
+                    return redirect('login')
+
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+    # request.method == GET
     else:
         return render(request, 'accounts/register.html')
 
--- real_estate/real_estate/static/js/main.js
+++ real_estate/real_estate/static/js/main.js
@@ -1,6 +1,7 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
 
+// FadeOut the error message for 3 seconds
 setTimeout(function () {
     $('#message').fadeOut('slow')
 }, 3000);
