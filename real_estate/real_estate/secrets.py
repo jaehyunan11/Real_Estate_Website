01c1285791b4d453ea48aa8d5a8d16bbd938d271
JaeHyun An <36184435+jaehyunan11@users.noreply.github.com>
2021-05-05 16:19:19 -0700

    Update secrets.py

--- real_estate/real_estate/secrets.py
+++ real_estate/real_estate/secrets.py
@@ -1,3 +1,3 @@
 secret_key = '9hp$9j05-ny!&af!ckdixr1eqr)g9mmb+9-o3&$*uhcy2=l3ss'
-db_password = 'jaeHYUN^^48625'
-email_password = 'jaeHYUN^^48625'
+db_password = '' # Please ask to admin
+email_password = '' # Please ask to admin
